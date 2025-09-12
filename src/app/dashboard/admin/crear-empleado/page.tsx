544947b684da070f8e142d7d8359bdc540659d64
Carlos-Dev-13 <carlos.calderon@goexpertis.com>
2025-09-12 08:53:32 -0500

    feat: probando el despligue v2.0

--- src/app/dashboard/admin/crear-empleado/page.tsx
+++ src/app/dashboard/admin/crear-empleado/page.tsx
@@ -1,9 +1,8 @@
 import { DashboardLayout } from "@/components/dashboard-layout";
-
 export default function CrearEmpleado(){
     return (
         <DashboardLayout>
-        <div>page</div>
+        <div>REGISTRO DE ASESORES</div>
         </DashboardLayout>
     )
 }
