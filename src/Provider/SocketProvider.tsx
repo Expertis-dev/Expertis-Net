fbcfcafd078441f465f7f3765459921707cd72c2
Carlos-Dev-13 <carlos.calderon@goexpertis.com>
2025-12-15 15:15:16 -0500

    feat: solucionando el problema del socket

--- src/Provider/SocketProvider.tsx
+++ src/Provider/SocketProvider.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 import { SocketContext } from "@/Context/SocketContex";
 import { useSocket } from "@/hooks/useSocket";
 export const SocketProvider = ({ children }: { children: ReactNode }) => {
-    const { socket, online } = useSocket("https://tb01wj9l-5000.brs.devtunnels.ms/");
+    const { socket, online } = useSocket("https://1q91g6w4-5000.brs.devtunnels.ms/");
     return (
         <SocketContext.Provider value={{ socket, online }}>
             {children}
